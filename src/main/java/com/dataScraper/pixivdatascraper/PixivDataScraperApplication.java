bd5cb389092bef376ce997d6016575a11f4b794b
Aldrino Shaju <aldrinoshaju@gmail.com>
2022-08-14 11:59:43 +0530

    Initial commit

---
+++ src/main/java/com/dataScraper/pixivdatascraper/PixivDataScraperApplication.java
@@ -0,0 +1,13 @@
+package com.dataScraper.pixivdatascraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixivDataScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixivDataScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dataScraper/pixivdatascraper/PixivDataScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataScraper.pixivdatascraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixivDataScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
